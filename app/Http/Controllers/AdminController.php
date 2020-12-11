52bcb4d259c416130958b4625a99d9a69d053dd5
unknown <rashadkhandoker@gmail.com>
2020-12-12 05:18:46 +0600

    first commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Message;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Pusher\Pusher;
+
+class AdminController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        // select all users except logged in user
+        // $users = User::where('id', '!=', Auth::id())->get();
+
+        // count how many message are unread from the selected user
+        return view('admin.home');
+    }
+
+   
+}
